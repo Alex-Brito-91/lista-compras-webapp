a42f09fbd41b29843bb0612fe95218b740c880e7
alex brito <alex.teixeira.brito@gmail.com>
2023-03-20 17:29:28 -0300

    alerta de preenchimento em cadastro vazio inserido no servlet

--- lista-compras-webapp/src/main/java/br/com/gerenciador/servlet/NovoProdutoServlet.java
+++ lista-compras-webapp/src/main/java/br/com/gerenciador/servlet/NovoProdutoServlet.java
@@ -1,6 +1,7 @@
 package br.com.gerenciador.servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.math.BigDecimal;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -13,26 +14,42 @@ public class NovoProdutoServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		System.out.println("cadastrando novo produto");
-		
-		String nome = request.getParameter("nome");
-		BigDecimal valorUnitario = new BigDecimal(request.getParameter("valor").replaceAll(",", "."));
-		Integer quantidade = Integer.parseInt(request.getParameter("quant"));
-		BigDecimal valorTotal = valorUnitario.multiply(BigDecimal.valueOf(quantidade));
-		
-		Produto produto = new Produto();
-		produto.setNome(nome);
-		produto.setValorUnitario(valorUnitario);
-		produto.setQuantidade(quantidade);
-		produto.setValorTotal(valorTotal);
-		
-		Banco banco = new Banco();
-		banco.adiciona(produto);
-		
-		response.setContentType("text/html; charset=UTF-8");
-		response.sendRedirect("paginaCadastroTotais");
-		
+
+	    String nome = request.getParameter("nome");
+	    BigDecimal valorUnitario = null;
+	    Integer quantidade = null;
+
+	    if (nome == null || nome.trim().isEmpty() || 
+	    		request.getParameter("nome") == null || request.getParameter("nome").trim().isEmpty() || 
+	            request.getParameter("valor") == null || request.getParameter("valor").trim().isEmpty() || 
+	            request.getParameter("quant") == null || request.getParameter("quant").trim().isEmpty()) {
+
+	        response.setContentType("text/html;charset=UTF-8");
+	        PrintWriter out = response.getWriter();
+	        out.println("<script type=\"text/javascript\">");
+	        out.println("alert('Por favor, preencha todos os campos antes de cadastrar!');");
+	        out.println("window.history.back();");
+	        out.println("</script>");
+
+	    } else {
+
+	        valorUnitario = new BigDecimal(request.getParameter("valor").replaceAll(",", "."));
+	        quantidade = Integer.parseInt(request.getParameter("quant"));
+	        BigDecimal valorTotal = valorUnitario.multiply(BigDecimal.valueOf(quantidade));
+	        Produto produto = new Produto();
+	        produto.setNome(nome);
+	        produto.setValorUnitario(valorUnitario);
+	        produto.setQuantidade(quantidade);
+	        produto.setValorTotal(valorTotal);
+
+	        Banco banco = new Banco();
+	        banco.adiciona(produto);
+
+	        response.setContentType("text/html; charset=UTF-8");
+	        response.sendRedirect("paginaCadastroTotais");
+
+	    }
 	}
+
 
 }
